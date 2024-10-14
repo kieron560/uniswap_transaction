819536b806988b1f23e0c59d401aabbfa76a1a14
kieron560 <kieron560@gmail.com>
2024-10-14 16:46:57 +0800

    complete basic logic for get transaction fee by hash

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import math
 from flask import Flask
 from flask import jsonify
 from flask import request
@@ -15,13 +16,21 @@ app = Flask(__name__)
 swagger = Swagger(app)
 
 
-@app.route("/transactions")
-def get_transaction_fee_by_hash():
-    key = os.getenv(constants.ETHERSCAN_API_KEY)
+@app.route("/transaction_fee")
+def get_transaction_fee():
     hash = request.args.get("hash")
     if hash is None:
         return ""
+    
+    internal_transaction_list = get_internal_transaction_list_by_hash(hash)
+    result = get_token_transfer_fee_by_hash(internal_transaction_list, hash)
 
+    return jsonify(result)
+
+
+def get_internal_transaction_list_by_hash(hash):
+    key = os.getenv(constants.ETHERSCAN_API_KEY)
+    
     payload = {
         "module": "account",
         "action": "txlistinternal",
@@ -31,10 +40,61 @@ def get_transaction_fee_by_hash():
 
     resp = requests.get(constants.ETHERSCAN_API_URL, payload)
     data = resp.json()
-    return data
+    return data["result"]
 
 
-# Configure Swagger UI
+def get_token_transfer_fee_by_hash(results, hash):
+    key = os.getenv(constants.ETHERSCAN_API_KEY)
+
+    for data in results:
+        timestamp = data["timeStamp"]
+        blockNumber = data["blockNumber"]
+        address = data["from"]
+        contractAddress = data["contractAddress"]
+
+        tokenPayload = {
+            "module": "account",
+            "action": "tokentx",
+            "address": address,
+            "apikey": key,
+            "startblock": blockNumber,
+            "endblock": blockNumber,
+            "sort": "asc"
+        }
+
+        pricePayload = {
+            "symbol": constants.ETH_USDT_SYMBOL,
+            "interval": "1s",
+            "startTime": unix_second_to_unix_millisecond(timestamp),
+            "endTime": unix_second_to_unix_millisecond(timestamp),
+        }
+
+        if contractAddress != "":
+            tokenPayload["contractAddress"] = contractAddress
+
+
+        priceResp = requests.get(constants.BINANCE_KLINES_API_URL, pricePayload)
+        priceData = priceResp.json()
+        price = priceData[0][4] # index 4 is the closing price
+
+        resp = requests.get(constants.ETHERSCAN_API_URL, tokenPayload)
+        respData = resp.json()
+        for r in respData["result"]:
+            if r["hash"] == hash and r["tokenSymbol"] == "WETH":
+                fee = calculate_transaction_fee_in_eth(r["gasPrice"], r["gasUsed"], r["tokenDecimal"])
+                feeInUSDT = fee * float(price)
+                return feeInUSDT
+
+
+def calculate_transaction_fee_in_eth(gas_price: str, gas_used: str, token_decimal: str):
+    return int(gas_price) * math.pow(10, -1 * int(token_decimal)) * int(gas_used)
+
+
+def unix_second_to_unix_millisecond(s):
+    return s + "000"
+
+
+# Configure Swagger UI for display
 SWAGGER_URL = '/swagger'
 API_URL = 'http://127.0.0.1:5000/swagger.json'
 swaggerui_blueprint = get_swaggerui_blueprint(
@@ -56,7 +116,6 @@ def swagger():
 
 if __name__ == "__main__":
     load_dotenv()
-
     print(("* Loading model and Flask starting server..."
            "please wait until server has fully started"))
     app.run(debug=True)
--- server/constants.py
+++ server/constants.py
@@ -4,3 +4,6 @@ This files contains all the related constants to be used, such as fixed API endp
 
 ETHERSCAN_API_KEY = "ETHERSCAN_API_KEY"
 ETHERSCAN_API_URL = "https://api.etherscan.io/api" 
+
+BINANCE_KLINES_API_URL = "https://api.binance.com/api/v3/klines"
+ETH_USDT_SYMBOL = "ETHUSDT"
